e3e8c10a6743bbc1bdd634ce4eb316988657cd5f
pmaxlauve <71564864+pmaxlauve@users.noreply.github.com>
2021-02-13 19:16:30 -0600

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -31,7 +31,13 @@ var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
 // Our AJAX call retrieves our earthquake geoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
 
+var quakeData = data.features;
 
+console.log(quakeData);
+
+var quakeCoords = quakeData.map(d => d.geometry.coordinates.slice(0,2));
+
+console.log(quakeCoords);
 
 
 
@@ -39,4 +45,11 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Here we make an AJAX call to get our Tectonic Plate geoJSON data.
   d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
     function(platedata) {
+      var platesData = platedata.features;
+      console.log(platesData)
 
+      var platePoly = platesData.map(d =>d.geometry.coordinates);
+
+      console.log(platePoly);
+
+    })})
